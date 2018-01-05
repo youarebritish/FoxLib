3a5d84eb07f945601c16dac2012c77dd39fe103c
youarebritish <blue.oblivion@gmail.com>
2018-01-05 15:49:40 -0800

    Revert "Marginal progress on TppRouteSet. Trying to figure out why unit tests disappeared."
    This reverts commit 634a5294517a74bcfaee83c863d0b6bc7429972e.

--- FoxLib/TppRouteSet.fs
+++ FoxLib/TppRouteSet.fs
@@ -2,40 +2,15 @@
 
 open FoxLib.Core
 
-type public IRouteEvent = interface end
-
-type public RouteEvent<'T1, 'T2, 'T3, 'T4, 'T5, 'T6, 'T7, 'T8, 'T9, 'T10> =
-    {   Type : StrCode32Hash
-        Param1 : 'T1
-        Param2 : 'T2
-        Param3 : 'T3
-        Param4 : 'T4
-        Param5 : 'T5
-        Param6 : 'T6
-        Param7 : 'T7
-        Param8 : 'T8
-        Param9 : 'T9
-        Param10 : 'T10
-        Snippet : string } interface IRouteEvent
-
 type public RouteNode = {
     Position : Vector3
-    EdgeEvent : IRouteEvent
-    Events : seq<IRouteEvent>
 }
 
 type public Route = {
     Name : StrCode32Hash
-    Nodes : seq<RouteNode>
+    Nodes : array<RouteNode>
 }
 
 type public RouteSet = {
-    Routes : seq<Route>
+    Routes : array<Route>
 }
-
-let public Read =
-    let routes = []
-    { Routes = routes }
-
-let public Write routeSet =
-    ()
