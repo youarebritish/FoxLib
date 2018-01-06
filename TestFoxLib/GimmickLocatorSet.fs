eb1005ce1cf9207c78758120754738a1096edee8
youarebritish <blue.oblivion@gmail.com>
2018-01-05 22:39:29 -0800

    Finished repairing GimmickLocatorSet unit tests after the C# API refactor.

--- TestFoxLib/GimmickLocatorSet.fs
+++ TestFoxLib/GimmickLocatorSet.fs
@@ -108,6 +108,75 @@ let private createRandomNamedGimmickLocator (random : System.Random) =
     let dataSetName = random.Next() |> uint32
     { Position = position; Rotation = rotation; LocatorName = locatorName; DataSetName = dataSetName }
 
+[<Test>]
+[<Category("GimmickLocatorSet")>]
+let ``empty NamedGimmickLocatorSet should be empty when read`` () =
+    let locatorSet = NamedGimmickLocatorSet([])
+
+    use stream = new MemoryStream()
+    use writer = new BinaryWriter(stream)    
+    GimmickLocatorSet.Write locatorSet (createWriteFunctions writer) |> ignore
+    
+    stream.Position <- 0L
+    
+    use reader = new BinaryReader(stream)
+    createReadFunctions reader
+    |> GimmickLocatorSet.Read
+    |> fun locatorSet ->
+        match locatorSet with
+        | NamedGimmickLocatorSet locators -> Seq.isEmpty locators |> Assert.IsTrue
+        | _ -> Assert.Fail()
+
+[<Test>]
+[<Category("GimmickLocatorSet")>]
+let ``one random NamedGimmickLocator should have original value when read`` () =
+    let random = new System.Random()
+    let locator = createRandomNamedGimmickLocator random
+    let locatorSet = NamedGimmickLocatorSet([locator])
+    
+    use stream = new MemoryStream()
+    use writer = new BinaryWriter(stream)
+    createWriteFunctions writer
+    |> GimmickLocatorSet.Write locatorSet
+    |> ignore
+        
+    stream.Position <- 0L
+
+    use reader = new BinaryReader(stream)
+    createReadFunctions reader
+    |> GimmickLocatorSet.Read
+    |> fun locatorSet ->    
+        match locatorSet with
+        | NamedGimmickLocatorSet locators ->
+            Seq.head locators
+            |> fun readLocator -> readLocator = locator |> Assert.IsTrue
+        | _ -> Assert.Fail()
+    
+[<Test>]
+[<Category("GimmickLocatorSet")>]
+let ``one hundred random NamedGimmickLocators should have original values when read`` () =
+    let random = new System.Random()
+    let locators = Array.init 100 (fun _ -> createRandomNamedGimmickLocator random)
+    let locatorSet = NamedGimmickLocatorSet(locators)
+    
+    use stream = new MemoryStream()
+    use writer = new BinaryWriter(stream)
+    createWriteFunctions writer
+    |> GimmickLocatorSet.Write locatorSet
+    |> ignore
+    
+    stream.Position <- 0L
+
+    use reader = new BinaryReader(stream)
+    createReadFunctions reader
+    |> GimmickLocatorSet.Read
+    |> fun locatorSet -> 
+        match locatorSet with
+        | NamedGimmickLocatorSet readLocators ->
+            let compareSequences = Seq.compareWith Operators.compare
+            compareSequences readLocators locators = 0 |> Assert.IsTrue
+        | _ -> Assert.Fail()
+
 let private createRandomScaledGimmickLocator (random : System.Random) =
     let makeFloat = random.NextDouble >> float32
 
@@ -117,3 +186,72 @@ let private createRandomScaledGimmickLocator (random : System.Random) =
     let locatorName = random.Next() |> uint32
     let dataSetName = random.Next() |> uint32
     { Position = position; Rotation = rotation; Scale = scale; LocatorName = locatorName; DataSetName = dataSetName }
+
+[<Test>]
+[<Category("GimmickLocatorSet")>]
+let ``empty ScaledGimmickLocatorSet should be empty when read`` () =
+    let locatorSet = ScaledGimmickLocatorSet([])
+
+    use stream = new MemoryStream()
+    use writer = new BinaryWriter(stream)    
+    GimmickLocatorSet.Write locatorSet (createWriteFunctions writer) |> ignore
+    
+    stream.Position <- 0L
+    
+    use reader = new BinaryReader(stream)
+    createReadFunctions reader
+    |> GimmickLocatorSet.Read
+    |> fun locatorSet ->
+        match locatorSet with
+        | ScaledGimmickLocatorSet locators -> Seq.isEmpty locators |> Assert.IsTrue
+        | _ -> Assert.Fail()
+
+[<Test>]
+[<Category("GimmickLocatorSet")>]
+let ``one random ScaledGimmickLocator should have original value when read`` () =
+    let random = new System.Random()
+    let locator = createRandomScaledGimmickLocator random
+    let locatorSet = ScaledGimmickLocatorSet([locator])
+    
+    use stream = new MemoryStream()
+    use writer = new BinaryWriter(stream)
+    createWriteFunctions writer
+    |> GimmickLocatorSet.Write locatorSet
+    |> ignore
+        
+    stream.Position <- 0L
+
+    use reader = new BinaryReader(stream)
+    createReadFunctions reader
+    |> GimmickLocatorSet.Read
+    |> fun locatorSet ->    
+        match locatorSet with
+        | ScaledGimmickLocatorSet locators ->
+            Seq.head locators
+            |> fun readLocator -> readLocator = locator |> Assert.IsTrue
+        | _ -> Assert.Fail()
+    
+[<Test>]
+[<Category("GimmickLocatorSet")>]
+let ``one hundred random ScaledGimmickLocators should have original values when read`` () =
+    let random = new System.Random()
+    let locators = Array.init 100 (fun _ -> createRandomScaledGimmickLocator random)
+    let locatorSet = ScaledGimmickLocatorSet(locators)
+    
+    use stream = new MemoryStream()
+    use writer = new BinaryWriter(stream)
+    createWriteFunctions writer
+    |> GimmickLocatorSet.Write locatorSet
+    |> ignore
+    
+    stream.Position <- 0L
+
+    use reader = new BinaryReader(stream)
+    createReadFunctions reader
+    |> GimmickLocatorSet.Read
+    |> fun locatorSet -> 
+        match locatorSet with
+        | ScaledGimmickLocatorSet readLocators ->
+            let compareSequences = Seq.compareWith Operators.compare
+            compareSequences readLocators locators = 0 |> Assert.IsTrue
+        | _ -> Assert.Fail()
