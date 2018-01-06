f337c092a8f17bf90f8c19a0b775a1742bf898a0
youarebritish <blue.oblivion@gmail.com>
2018-01-05 22:35:34 -0800

    Refactored GimmickLocatorSet to use an idiomatic C# API for smooth interoperability. Need to restore some of the unit tests.

--- FoxLib/GimmickLocatorSet.fs
+++ FoxLib/GimmickLocatorSet.fs
@@ -3,6 +3,7 @@
 open System.IO
 open FoxLib
 open FoxLib.Tpp.GimmickLocator
+open System
     
 /// <summmary>
 /// A sequence of locators for gimmicks, indicating where they should be placed.
@@ -128,33 +129,69 @@ let private readGimmickLocatorSetBody readVector4 readQuaternion readWideVector3
         let readMetadata = fun () -> readLocatorMetadata readUInt32
         ScaledGimmickLocatorSet(readScaledGimmickLocators readTransform readMetadata locatorCount)
     | _ -> raise (new InvalidDataException("The file is not an .lba file or is an unrecognized format."))
-                  
+  
+/// <summmary>
+/// Input functions to the Read function.
+/// </summmary>
+type public ReadFunctions = {
+    /// Function to read a float32.
+    ReadSingle : Func<float32>
+    /// Function to read a uint16.
+    ReadUInt16 : Func<uint16>
+    /// Function to read a uint32.
+    ReadUInt32 : Func<uint32>
+    /// Function to read a int32.
+    ReadInt32 : Func<int32>
+    /// Function to skip a number of bytes.
+    SkipBytes : Action<int>
+}
+
+/// <summmary>
+/// Read parameters converted to F# functions.
+/// </summmary>
+type private ConvertedReadFunctions = {
+    ReadSingle : unit -> float32
+    ReadUInt16 : unit -> uint16
+    ReadUInt32 : unit -> uint32
+    ReadInt32 : unit -> int32
+    SkipBytes : int -> unit
+}
+
+/// <summmary>
+/// Converts the Read function's .NET Funcs into F# functions.
+/// </summmary>
+/// <param name="rawReadFunctions">Input functions supplied to the Read function.</param>
+/// <returns>The converted functions.</returns>
+let private convertReadFunctions (rawReadFunctions : ReadFunctions) =
+    { ConvertedReadFunctions.ReadSingle = rawReadFunctions.ReadSingle.Invoke;
+    ReadUInt16 = rawReadFunctions.ReadUInt16.Invoke;
+    ReadUInt32 = rawReadFunctions.ReadUInt32.Invoke;
+    ReadInt32 = rawReadFunctions.ReadInt32.Invoke;
+    SkipBytes = rawReadFunctions.SkipBytes.Invoke; }
+
 /// <summmary>
 /// Parses a GimmickLocatorSet from lba format.
 /// </summmary>
-/// <param name="readSingle">Function to read a float32.</param>
-/// <param name="readUInt16">Function to read a uint16.</param>
-/// <param name="readUInt32">Function to read a uint32.</param>
-/// <param name="readInt32">Function to read a int32.</param>
-/// <param name="skipBytes">Function to skip a number of bytes.</param>
+/// <param name="readFunctions">Functions to read various data types from the input.</param>
 /// <returns>The parsed GimmickLocatorSet.</returns>
-let public Read readSingle readUInt16 readUInt32 readInt32 skipBytes =
-    let locatorCount = readInt32()
-    let format = readInt32()  |> enum<GimmickLocatorSetFormat>
-    skipBytes 8 |> ignore
+let public Read (readFunctions : ReadFunctions) =
+    let convertedFunctions = convertReadFunctions readFunctions
+    let locatorCount = convertedFunctions.ReadInt32()
+    let format = convertedFunctions.ReadInt32()  |> enum<GimmickLocatorSetFormat>
+    convertedFunctions.SkipBytes 8 |> ignore
 
-    let readVector4 = fun () -> Vector4.Read readSingle
-    let readQuaternion = fun () -> Quaternion.Read readSingle
-    let readWideVector3 = fun () -> WideVector3.Read readSingle readUInt16    
-    readGimmickLocatorSetBody readVector4 readQuaternion readWideVector3 readUInt32 format locatorCount
+    let readVector4 = fun () -> Vector4.Read convertedFunctions.ReadSingle
+    let readQuaternion = fun () -> Quaternion.Read convertedFunctions.ReadSingle
+    let readWideVector3 = fun () -> WideVector3.Read convertedFunctions.ReadSingle convertedFunctions.ReadUInt16    
+    readGimmickLocatorSetBody readVector4 readQuaternion readWideVector3 convertedFunctions.ReadUInt32 format locatorCount
 
 /// <summary>
 /// Writes a GimmickLocatorSet's header.
 /// </summary>
-/// <param name="writeInt32">Functoin to write an int32.</param>
-/// <param name="writeEmptyBytes"></param>
-/// <param name="locatorCount"></param>
-/// <param name="format"></param>
+/// <param name="writeInt32">Function to write an int32.</param>
+/// <param name="writeEmptyBytes">Function to write a number of empty bytes.</param>
+/// <param name="locatorCount">Number of locators in the locator set.</param>
+/// <param name="format">Format of the locator set.</param>
 let private writeGimmickLocatorSetHeader writeInt32 writeEmptyBytes locatorCount format =
     writeInt32 locatorCount
     LanguagePrimitives.EnumToValue format |> writeInt32
@@ -231,29 +268,65 @@ let private writeScaledGimmickLocatorSet writeTransform writeMetadata locators =
         locator.LocatorName, locator.DataSetName)
     |> Array.iter (fun locatorMetadata -> writeMetadata locatorMetadata)
 
+/// <summmary>
+/// Input functions to the Write function.
+/// </summmary>
+type public WriteFunctions = {
+    /// Function to write a float32.
+    WriteSingle : Action<float32>
+    /// Function to write a uint16.
+    WriteUInt16 : Action<uint16>
+    /// Function to write a uint32.
+    WriteUInt32 : Action<uint32>
+    /// Function to write a int32.
+    WriteInt32 : Action<int32>
+    /// Function to write a number of filler bytes.
+    WriteEmptyBytes : Action<int>
+}
+
+/// <summmary>
+/// Write parameters converted to F# functions.
+/// </summmary>
+type private ConvertedWriteFunctions = {
+    WriteSingle : float32 -> unit
+    WriteUInt16 : uint16 -> unit
+    WriteUInt32 : uint32 -> unit
+    WriteInt32 : int32 -> unit
+    WriteEmptyBytes : int32 -> unit
+}
+
+/// <summmary>
+/// Converts the Write function's .NET Funcs into F# functions.
+/// </summmary>
+/// <param name="rawWriteFunctions">Input functions supplied to the Write function.</param>
+/// <returns>The converted functions.</returns>
+let private convertWriteFunctions (rawWriteFunctions : WriteFunctions) =
+    { ConvertedWriteFunctions.WriteSingle = rawWriteFunctions.WriteSingle.Invoke;
+    WriteUInt16 = rawWriteFunctions.WriteUInt16.Invoke;
+    WriteUInt32 = rawWriteFunctions.WriteUInt32.Invoke;
+    WriteInt32 = rawWriteFunctions.WriteInt32.Invoke;
+    WriteEmptyBytes = rawWriteFunctions.WriteEmptyBytes.Invoke; }
+
 /// <summary>
 /// Writes a GimmickLocatorSet to lba format.
 /// </summary>
-/// <param name="writeSingle">Function to write a float32.</param>
-/// <param name="writeUInt16">Function to write a uint16.</param>
-/// <param name="writeInt32">Function to write a int32.</param>
-/// <param name="writeUInt32">Function to write a uint32.</param>
-/// <param name="writeEmptyBytes">Function to write a number of empty bytes.</param>
+/// <param name="writeFunctions">Function to write various data types.</param>
 /// <param name="locatorSet">GimmickLocatorSet to write.</param>
-let public Write writeSingle writeUInt16 writeInt32 writeUInt32 writeEmptyBytes locatorSet =
-    let writeHeader = writeGimmickLocatorSetHeader writeInt32 writeEmptyBytes
+let public Write locatorSet (writeFunctions : WriteFunctions) =
+    let convertedWriteFunctions = convertWriteFunctions writeFunctions
+    let writeHeader = writeGimmickLocatorSetHeader convertedWriteFunctions.WriteInt32 convertedWriteFunctions.WriteEmptyBytes
     match locatorSet with
     | PowerCutAreaGimmickLocatorSet locators ->
         writeHeader (Seq.length locators) GimmickLocatorSetFormat.PowerCutArea
-        let writeTransform = writeUnscaledGimmickLocatorTransform writeSingle
+        let writeTransform = writeUnscaledGimmickLocatorTransform convertedWriteFunctions.WriteSingle
         writePowerCutAreaGimmickLocatorSet writeTransform (Seq.toArray locators)
     | NamedGimmickLocatorSet locators ->
         writeHeader (Seq.length locators) GimmickLocatorSetFormat.Named
-        let writeTransform = writeUnscaledGimmickLocatorTransform writeSingle
-        let writeMetadata = writeGimmickLocatorMetadata writeUInt32
+        let writeTransform = writeUnscaledGimmickLocatorTransform convertedWriteFunctions.WriteSingle
+        let writeMetadata = writeGimmickLocatorMetadata convertedWriteFunctions.WriteUInt32
         writeNamedGimmickLocatorSet writeTransform writeMetadata (Seq.toArray locators)
     | ScaledGimmickLocatorSet locators ->
         writeHeader (Seq.length locators) GimmickLocatorSetFormat.Scaled
-        let writeTransform = writeScaledGimmickLocatorTransform writeSingle writeUInt16
-        let writeMetadata = writeGimmickLocatorMetadata writeUInt32
+        let writeTransform = writeScaledGimmickLocatorTransform convertedWriteFunctions.WriteSingle convertedWriteFunctions.WriteUInt16
+        let writeMetadata = writeGimmickLocatorMetadata convertedWriteFunctions.WriteUInt32
         writeScaledGimmickLocatorSet writeTransform writeMetadata (Seq.toArray locators)
--- TestFoxLib/GimmickLocatorSet.fs
+++ TestFoxLib/GimmickLocatorSet.fs
@@ -7,6 +7,7 @@ open FoxLib.Tpp.GimmickLocatorSet
 open NUnit.Framework
 open System.IO
 open FoxLib.Tpp
+open System
 
 let private createRandomPowerCutAreaGimmickLocator (random : System.Random) =
     let makeFloat = random.NextDouble >> float32
@@ -14,28 +15,39 @@ let private createRandomPowerCutAreaGimmickLocator (random : System.Random) =
     let position = { Vector4.X = makeFloat(); Y = float32 <| makeFloat(); Z = float32 <| makeFloat(); W = float32 <| makeFloat() }
     let rotation = { Quaternion.X = makeFloat(); Y = makeFloat(); Z = makeFloat(); W = makeFloat() }
     { Position = position; Rotation = rotation }
+
+let private createWriteFunctions (writer : BinaryWriter) =
+    { WriteFunctions.WriteSingle = new Action<float32>(writer.Write);
+    WriteUInt16 = new Action<uint16>(writer.Write);
+    WriteUInt32 = new Action<uint32>(writer.Write);
+    WriteInt32 = new Action<int32>(writer.Write);
+    WriteEmptyBytes = new Action<int>(fun numBytes -> Array.zeroCreate<byte> numBytes |> writer.Write) }
+
+let private createReadFunctions (reader : BinaryReader) =
+    { ReadFunctions.ReadSingle = new Func<float32>(reader.ReadSingle);
+    ReadUInt16 = new Func<uint16>(reader.ReadUInt16);
+    ReadUInt32 = new Func<uint32>(reader.ReadUInt32);
+    ReadInt32 = new Func<int32>(reader.ReadInt32);
+    SkipBytes = new Action<int>(fun numBytes -> reader.ReadBytes numBytes |> ignore) }
         
 [<Test>]
 [<Category("GimmickLocatorSet")>]
 let ``empty PowerCutAreaGimmickLocatorSet should be empty when read`` () =
-    let locators = Array.empty
-    let locatorSet = PowerCutAreaGimmickLocatorSet(locators)
+    let locatorSet = PowerCutAreaGimmickLocatorSet([])
 
     use stream = new MemoryStream()
-    use writer = new BinaryWriter(stream)
-    let writeEmptyBytes numBytes = Array.zeroCreate<byte> numBytes |> writer.Write
-
-    GimmickLocatorSet.Write writer.Write writer.Write writer.Write writer.Write writeEmptyBytes locatorSet
+    use writer = new BinaryWriter(stream)    
+    GimmickLocatorSet.Write locatorSet (createWriteFunctions writer) |> ignore
     
     stream.Position <- 0L
-
-    use reader = new BinaryReader(stream)
-    let skipBytes numBytes = reader.ReadBytes numBytes
-    let locatorSet = GimmickLocatorSet.Read reader.ReadSingle reader.ReadUInt16 reader.ReadUInt32 reader.ReadInt32 skipBytes
     
-    match locatorSet with
-    | PowerCutAreaGimmickLocatorSet locators -> Seq.isEmpty locators |> Assert.IsTrue
-    | _ -> Assert.Fail()
+    use reader = new BinaryReader(stream)
+    createReadFunctions reader
+    |> GimmickLocatorSet.Read
+    |> fun locatorSet ->
+        match locatorSet with
+        | PowerCutAreaGimmickLocatorSet locators -> Seq.isEmpty locators |> Assert.IsTrue
+        | _ -> Assert.Fail()
 
 [<Test>]
 [<Category("GimmickLocatorSet")>]
@@ -46,21 +58,21 @@ let ``one random PowerCutAreaGimmickLocator should have original value when read
     
     use stream = new MemoryStream()
     use writer = new BinaryWriter(stream)
-    let writeEmptyBytes numBytes = Array.zeroCreate<byte> numBytes |> writer.Write
-
-    GimmickLocatorSet.Write writer.Write writer.Write writer.Write writer.Write writeEmptyBytes locatorSet
-    
+    createWriteFunctions writer
+    |> GimmickLocatorSet.Write locatorSet
+    |> ignore
+        
     stream.Position <- 0L
 
     use reader = new BinaryReader(stream)
-    let skipBytes numBytes = reader.ReadBytes numBytes
-    let locatorSet = GimmickLocatorSet.Read reader.ReadSingle reader.ReadUInt16 reader.ReadUInt32 reader.ReadInt32 skipBytes
-    
-    match locatorSet with
-    | PowerCutAreaGimmickLocatorSet locators ->
-        Seq.head locators
-        |> fun readLocator -> readLocator = locator |> Assert.IsTrue
-    | _ -> Assert.Fail()
+    createReadFunctions reader
+    |> GimmickLocatorSet.Read
+    |> fun locatorSet ->    
+        match locatorSet with
+        | PowerCutAreaGimmickLocatorSet locators ->
+            Seq.head locators
+            |> fun readLocator -> readLocator = locator |> Assert.IsTrue
+        | _ -> Assert.Fail()
     
 [<Test>]
 [<Category("GimmickLocatorSet")>]
@@ -71,21 +83,21 @@ let ``one hundred random PowerCutAreaGimmickLocators should have original values
     
     use stream = new MemoryStream()
     use writer = new BinaryWriter(stream)
-    let writeEmptyBytes numBytes = Array.zeroCreate<byte> numBytes |> writer.Write
-
-    GimmickLocatorSet.Write writer.Write writer.Write writer.Write writer.Write writeEmptyBytes locatorSet
+    createWriteFunctions writer
+    |> GimmickLocatorSet.Write locatorSet
+    |> ignore
     
     stream.Position <- 0L
 
     use reader = new BinaryReader(stream)
-    let skipBytes numBytes = reader.ReadBytes numBytes
-    let locatorSet = GimmickLocatorSet.Read reader.ReadSingle reader.ReadUInt16 reader.ReadUInt32 reader.ReadInt32 skipBytes
-    
-    match locatorSet with
-    | PowerCutAreaGimmickLocatorSet readLocators ->
-        let compareSequences = Seq.compareWith Operators.compare
-        compareSequences readLocators locators = 0 |> Assert.IsTrue
-    | _ -> Assert.Fail()
+    createReadFunctions reader
+    |> GimmickLocatorSet.Read
+    |> fun locatorSet -> 
+        match locatorSet with
+        | PowerCutAreaGimmickLocatorSet readLocators ->
+            let compareSequences = Seq.compareWith Operators.compare
+            compareSequences readLocators locators = 0 |> Assert.IsTrue
+        | _ -> Assert.Fail()
 
 let private createRandomNamedGimmickLocator (random : System.Random) =
     let makeFloat = random.NextDouble >> float32
@@ -95,78 +107,6 @@ let private createRandomNamedGimmickLocator (random : System.Random) =
     let locatorName = random.Next() |> uint32
     let dataSetName = random.Next() |> uint32
     { Position = position; Rotation = rotation; LocatorName = locatorName; DataSetName = dataSetName }
-        
-[<Test>]
-[<Category("GimmickLocatorSet")>]
-let ``empty NamedGimmickLocatorSet should be empty when read`` () =
-    let locators = Array.empty
-    let locatorSet = NamedGimmickLocatorSet(locators)
-
-    use stream = new MemoryStream()
-    use writer = new BinaryWriter(stream)
-    let writeEmptyBytes numBytes = Array.zeroCreate<byte> numBytes |> writer.Write
-
-    GimmickLocatorSet.Write writer.Write writer.Write writer.Write writer.Write writeEmptyBytes locatorSet
-    
-    stream.Position <- 0L
-
-    use reader = new BinaryReader(stream)
-    let skipBytes numBytes = reader.ReadBytes numBytes
-    let locatorSet = GimmickLocatorSet.Read reader.ReadSingle reader.ReadUInt16 reader.ReadUInt32 reader.ReadInt32 skipBytes
-    
-    match locatorSet with
-    | NamedGimmickLocatorSet locators -> Seq.isEmpty locators |> Assert.IsTrue
-    | _ -> Assert.Fail()
-
-[<Test>]
-[<Category("GimmickLocatorSet")>]
-let ``one random NamedGimmickLocator should have original value when read`` () =
-    let random = new System.Random()
-    let locator = createRandomNamedGimmickLocator random
-    let locatorSet = NamedGimmickLocatorSet([locator])
-    
-    use stream = new MemoryStream()
-    use writer = new BinaryWriter(stream)
-    let writeEmptyBytes numBytes = Array.zeroCreate<byte> numBytes |> writer.Write
-
-    GimmickLocatorSet.Write writer.Write writer.Write writer.Write writer.Write writeEmptyBytes locatorSet
-    
-    stream.Position <- 0L
-
-    use reader = new BinaryReader(stream)
-    let skipBytes numBytes = reader.ReadBytes numBytes
-    let locatorSet = GimmickLocatorSet.Read reader.ReadSingle reader.ReadUInt16 reader.ReadUInt32 reader.ReadInt32 skipBytes
-    
-    match locatorSet with
-    | NamedGimmickLocatorSet locators ->
-        Seq.head locators
-        |> fun readLocator -> readLocator = locator |> Assert.IsTrue
-    | _ -> Assert.Fail()
-    
-[<Test>]
-[<Category("GimmickLocatorSet")>]
-let ``one hundred random NamedGimmickLocators should have original values when read`` () =
-    let random = new System.Random()
-    let locators = Array.init 100 (fun _ -> createRandomNamedGimmickLocator random)
-    let locatorSet = NamedGimmickLocatorSet(locators)
-    
-    use stream = new MemoryStream()
-    use writer = new BinaryWriter(stream)
-    let writeEmptyBytes numBytes = Array.zeroCreate<byte> numBytes |> writer.Write
-
-    GimmickLocatorSet.Write writer.Write writer.Write writer.Write writer.Write writeEmptyBytes locatorSet
-    
-    stream.Position <- 0L
-
-    use reader = new BinaryReader(stream)
-    let skipBytes numBytes = reader.ReadBytes numBytes
-    let locatorSet = GimmickLocatorSet.Read reader.ReadSingle reader.ReadUInt16 reader.ReadUInt32 reader.ReadInt32 skipBytes
-    
-    match locatorSet with
-    | NamedGimmickLocatorSet readLocators ->
-        let compareSequences = Seq.compareWith Operators.compare
-        compareSequences readLocators locators = 0 |> Assert.IsTrue
-    | _ -> Assert.Fail()
 
 let private createRandomScaledGimmickLocator (random : System.Random) =
     let makeFloat = random.NextDouble >> float32
@@ -177,75 +117,3 @@ let private createRandomScaledGimmickLocator (random : System.Random) =
     let locatorName = random.Next() |> uint32
     let dataSetName = random.Next() |> uint32
     { Position = position; Rotation = rotation; Scale = scale; LocatorName = locatorName; DataSetName = dataSetName }
-        
-[<Test>]
-[<Category("GimmickLocatorSet")>]
-let ``empty ScaledGimmickLocatorSet should be empty when read`` () =
-    let locators = Array.empty
-    let locatorSet = ScaledGimmickLocatorSet(locators)
-
-    use stream = new MemoryStream()
-    use writer = new BinaryWriter(stream)
-    let writeEmptyBytes numBytes = Array.zeroCreate<byte> numBytes |> writer.Write
-
-    GimmickLocatorSet.Write writer.Write writer.Write writer.Write writer.Write writeEmptyBytes locatorSet
-    
-    stream.Position <- 0L
-
-    use reader = new BinaryReader(stream)
-    let skipBytes numBytes = reader.ReadBytes numBytes
-    let locatorSet = GimmickLocatorSet.Read reader.ReadSingle reader.ReadUInt16 reader.ReadUInt32 reader.ReadInt32 skipBytes
-    
-    match locatorSet with
-    | ScaledGimmickLocatorSet locators -> Seq.isEmpty locators |> Assert.IsTrue
-    | _ -> Assert.Fail()
-
-[<Test>]
-[<Category("GimmickLocatorSet")>]
-let ``one random ScaledGimmickLocator should have original value when read`` () =
-    let random = new System.Random()
-    let locator = createRandomScaledGimmickLocator random
-    let locatorSet = ScaledGimmickLocatorSet([locator])
-    
-    use stream = new MemoryStream()
-    use writer = new BinaryWriter(stream)
-    let writeEmptyBytes numBytes = Array.zeroCreate<byte> numBytes |> writer.Write
-
-    GimmickLocatorSet.Write writer.Write writer.Write writer.Write writer.Write writeEmptyBytes locatorSet
-    
-    stream.Position <- 0L
-
-    use reader = new BinaryReader(stream)
-    let skipBytes numBytes = reader.ReadBytes numBytes
-    let locatorSet = GimmickLocatorSet.Read reader.ReadSingle reader.ReadUInt16 reader.ReadUInt32 reader.ReadInt32 skipBytes
-    
-    match locatorSet with
-    | ScaledGimmickLocatorSet locators ->
-        Seq.head locators
-        |> fun readLocator -> readLocator = locator |> Assert.IsTrue
-    | _ -> Assert.Fail()
-    
-[<Test>]
-[<Category("GimmickLocatorSet")>]
-let ``one hundred random ScaledGimmickLocators should have original values when read`` () =
-    let random = new System.Random()
-    let locators = Array.init 100 (fun _ -> createRandomScaledGimmickLocator random)
-    let locatorSet = ScaledGimmickLocatorSet(locators)
-    
-    use stream = new MemoryStream()
-    use writer = new BinaryWriter(stream)
-    let writeEmptyBytes numBytes = Array.zeroCreate<byte> numBytes |> writer.Write
-
-    GimmickLocatorSet.Write writer.Write writer.Write writer.Write writer.Write writeEmptyBytes locatorSet
-    
-    stream.Position <- 0L
-
-    use reader = new BinaryReader(stream)
-    let skipBytes numBytes = reader.ReadBytes numBytes
-    let locatorSet = GimmickLocatorSet.Read reader.ReadSingle reader.ReadUInt16 reader.ReadUInt32 reader.ReadInt32 skipBytes
-    
-    match locatorSet with
-    | ScaledGimmickLocatorSet readLocators ->
-        let compareSequences = Seq.compareWith Operators.compare
-        compareSequences readLocators locators = 0 |> Assert.IsTrue
-    | _ -> Assert.Fail()
