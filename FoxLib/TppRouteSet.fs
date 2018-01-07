75619ad4ae8a58eac77a25d86f34b0d1c263bd5a
youarebritish <blue.oblivion@gmail.com>
2018-01-06 21:12:23 -0800

    Beginning infrastructure to read/write frt files.

--- FoxLib/TppRouteSet.fs
+++ FoxLib/TppRouteSet.fs
@@ -1,6 +1,7 @@
 ﻿module FoxLib.Tpp.RouteSet
 
 open FoxLib.Core
+open System
 
 type public IRouteEvent = interface end
 
@@ -33,9 +34,121 @@ type public RouteSet = {
     Routes : seq<Route>
 }
 
-let public Read =
+/// <summmary>
+/// Input functions to the Read function.
+/// </summmary>
+type public ReadFunctions = {
+    /// Function to read a float32.
+    ReadSingle : Func<float32>
+    /// Function to read a uint16.
+    ReadUInt16 : Func<uint16>
+    /// Function to read a uint32.
+    ReadUInt32 : Func<uint32>
+    /// Function to read a int32.
+    ReadInt32 : Func<int32>
+    /// Function to read a char.
+    ReadChar : Func<char>
+    /// Function to skip a number of bytes.
+    SkipBytes : Action<int>
+}
+
+/// <summmary>
+/// Read parameters converted to F# functions.
+/// </summmary>
+type private ConvertedReadFunctions = {
+    ReadSingle : unit -> float32
+    ReadUInt16 : unit -> uint16
+    ReadUInt32 : unit -> uint32
+    ReadInt32 : unit -> int32
+    ReadChar : unit -> char
+    SkipBytes : int -> unit
+}
+
+/// <summmary>
+/// Converts the Read function's .NET Funcs into F# functions.
+/// </summmary>
+/// <param name="rawReadFunctions">Input functions supplied to the Read function.</param>
+/// <returns>The converted functions.</returns>
+let private convertReadFunctions (rawReadFunctions : ReadFunctions) =
+    if rawReadFunctions.ReadInt32 |> isNull then nullArg "ReadInt32"
+    if rawReadFunctions.ReadUInt16 |> isNull then nullArg "ReadUInt16"
+    if rawReadFunctions.ReadUInt32 |> isNull then nullArg "ReadUInt32"
+    if rawReadFunctions.ReadInt32 |> isNull then nullArg "ReadInt32"
+    if rawReadFunctions.ReadChar |> isNull then nullArg "ReadChar"
+    if rawReadFunctions.SkipBytes |> isNull then nullArg "SkipBytes"
+
+    { ConvertedReadFunctions.ReadSingle = rawReadFunctions.ReadSingle.Invoke;
+    ReadUInt16 = rawReadFunctions.ReadUInt16.Invoke;
+    ReadUInt32 = rawReadFunctions.ReadUInt32.Invoke;
+    ReadInt32 = rawReadFunctions.ReadInt32.Invoke;
+    ReadChar = rawReadFunctions.ReadChar.Invoke;
+    SkipBytes = rawReadFunctions.SkipBytes.Invoke; }
+
+/// <summmary>
+/// Parses a TppRouteSet from frt format.
+/// </summmary>
+/// <param name="readFunctions">Functions to read various data types from the input.</param>
+/// <returns>The parsed TppRouteSet.</returns>
+let public Read (readFunctions : ReadFunctions) =
+    let convertedFunctions = convertReadFunctions readFunctions
     let routes = []
     { Routes = routes }
 
-let public Write routeSet =
+/// <summmary>
+/// Input functions to the Write function.
+/// </summmary>
+type public WriteFunctions = {
+    /// Function to write a float32.
+    WriteSingle : Action<float32>
+    /// Function to write a uint16.
+    WriteUInt16 : Action<uint16>
+    /// Function to write a uint32.
+    WriteUInt32 : Action<uint32>
+    /// Function to write a int32.
+    WriteInt32 : Action<int32>
+    /// Function to write a char.
+    WriteChar : Action<char>
+    /// Function to write a number of filler bytes.
+    WriteEmptyBytes : Action<int>
+}
+
+/// <summmary>
+/// Write parameters converted to F# functions.
+/// </summmary>
+type private ConvertedWriteFunctions = {
+    WriteSingle : float32 -> unit
+    WriteUInt16 : uint16 -> unit
+    WriteUInt32 : uint32 -> unit
+    WriteInt32 : int32 -> unit
+    WriteChar : char -> unit
+    WriteEmptyBytes : int32 -> unit
+}
+
+/// <summmary>
+/// Converts the Write function's .NET Funcs into F# functions.
+/// </summmary>
+/// <param name="rawWriteFunctions">Input functions supplied to the Write function.</param>
+/// <returns>The converted functions.</returns>
+let private convertWriteFunctions (rawWriteFunctions : WriteFunctions) =
+    if rawWriteFunctions.WriteSingle |> isNull then nullArg "WriteSingle"
+    if rawWriteFunctions.WriteUInt16 |> isNull then nullArg "WriteUInt16"
+    if rawWriteFunctions.WriteUInt32 |> isNull then nullArg "WriteUInt32"
+    if rawWriteFunctions.WriteInt32 |> isNull then nullArg "WriteInt32"
+    if rawWriteFunctions.WriteChar |> isNull then nullArg "WriteChar"
+    if rawWriteFunctions.WriteEmptyBytes |> isNull then nullArg "WriteEmptyBytes"
+
+    { ConvertedWriteFunctions.WriteSingle = rawWriteFunctions.WriteSingle.Invoke;
+    WriteUInt16 = rawWriteFunctions.WriteUInt16.Invoke;
+    WriteUInt32 = rawWriteFunctions.WriteUInt32.Invoke;
+    WriteInt32 = rawWriteFunctions.WriteInt32.Invoke;
+    WriteChar = rawWriteFunctions.WriteChar.Invoke;
+    WriteEmptyBytes = rawWriteFunctions.WriteEmptyBytes.Invoke; }
+
+/// <summary>
+/// Writes a TppRouteSet to frt format.
+/// </summary>
+/// <param name="writeFunctions">Function to write various data types.</param>
+/// <param name="locatorSet">TppRouteSet to write.</param>
+let public Write routeSet (writeFunctions : WriteFunctions) =
+    let convertedWriteFunctions = convertWriteFunctions writeFunctions
     ()
--- TestFoxLib/TppRouteSet.fs
+++ TestFoxLib/TppRouteSet.fs
@@ -1,9 +1,28 @@
 ﻿module TppRouteSet
 
-open FoxLib.Tpp
-open FoxLib.Tpp.RouteSet
-open System.IO
 open NUnit.Framework
+open System.IO
+open System
+
+open FoxLib.Tpp.RouteSet
+open FoxLib.Tpp
+open FoxLib.Core
+
+let private createWriteFunctions (writer : BinaryWriter) =
+    { WriteFunctions.WriteSingle = new Action<float32>(writer.Write);
+    WriteUInt16 = new Action<uint16>(writer.Write);
+    WriteUInt32 = new Action<uint32>(writer.Write);
+    WriteInt32 = new Action<int32>(writer.Write);
+    WriteChar = new Action<char>(writer.Write);
+    WriteEmptyBytes = new Action<int>(fun numBytes -> Array.zeroCreate<byte> numBytes |> writer.Write) }
+
+let private createReadFunctions (reader : BinaryReader) =
+    { ReadFunctions.ReadSingle = new Func<float32>(reader.ReadSingle);
+    ReadUInt16 = new Func<uint16>(reader.ReadUInt16);
+    ReadUInt32 = new Func<uint32>(reader.ReadUInt32);
+    ReadInt32 = new Func<int32>(reader.ReadInt32);
+    ReadChar = new Func<char>(reader.ReadChar);
+    SkipBytes = new Action<int>(fun numBytes -> reader.ReadBytes numBytes |> ignore) }
 
 [<Test>]
 [<Category("TppRouteSet")>]
@@ -11,11 +30,81 @@ let ``empty TppRouteSet should be empty when read`` () =
     let routeSet = { Routes = [] }
     use stream = new MemoryStream()
     use writer = new BinaryWriter(stream)
-    RouteSet.Write routeSet
+    RouteSet.Write routeSet (createWriteFunctions writer) |> ignore
 
     stream.Position <- 0L
 
     use reader = new BinaryReader(stream)
-    let readRouteSet = RouteSet.Read
+    createReadFunctions reader
+    |> RouteSet.Read
+    |> fun routeSet -> Seq.isEmpty routeSet.Routes |> Assert.IsTrue
 
-    Seq.isEmpty routeSet.Routes |> Assert.IsTrue
+let private createRandomRouteEvent (random : System.Random) =
+   { Type = random.Next() |> uint32;
+    Param1 = random.Next();
+    Param2 = random.Next();
+    Param3 = random.Next();
+    Param4 = random.Next();
+    Param5 = random.Next();
+    Param6 = random.Next();
+    Param7 = random.Next();
+    Param8 = random.Next();
+    Param9 = random.Next();
+    Param10 = random.Next();
+    Snippet = "test" } :> IRouteEvent
+
+let private createRandomRouteNode (random : System.Random) eventCount =
+    let makeFloat = random.NextDouble >> float32
+    let position = { Vector3.X = makeFloat(); Y = float32 <| makeFloat(); Z = float32 <| makeFloat() }
+    let edgeEvent = createRandomRouteEvent random
+    let (events : seq<IRouteEvent>) = [0..eventCount - 1] |> Seq.map (fun _ -> createRandomRouteEvent random)
+    { Position = position; EdgeEvent = edgeEvent; Events = events }
+
+let private createRandomRoute (random : System.Random) nodeCount eventsPerNode =
+    { Name = random.Next() |> uint32;
+    Nodes = [0..nodeCount - 1] |> Seq.map (fun _ -> createRandomRouteNode random eventsPerNode) }
+
+let private createRandomRouteSet (random : System.Random) routeCount nodesPerRoute eventsPerNode =
+    { Routes = [0..routeCount - 1] |> Seq.map (fun _ -> createRandomRoute random nodesPerRoute eventsPerNode) }
+
+let private areNodeEventSetsIdentical (nodeEventsA : seq<IRouteEvent>) (nodeEventsB : seq<IRouteEvent>) =
+    [0 .. Seq.length nodeEventsA]
+    |> Seq.exists (fun i ->
+         (Seq.item i nodeEventsA) = (Seq.item i nodeEventsB))
+
+let private areNodesIdentical (nodeA : RouteNode) (nodeB : RouteNode) =
+    nodeA.Position = nodeB.Position
+    && nodeA.EdgeEvent = nodeB.EdgeEvent
+    && [0 .. Seq.length nodeA.Events - 1]
+        |> Seq.exists (fun i ->
+            not <| areNodeEventSetsIdentical nodeA.Events nodeB.Events)
+        |> not
+
+let private areRoutesIdentical routeA routeB =
+    [0 .. Seq.length routeA.Nodes - 1]
+    |> Seq.exists (fun i ->
+        not <| areNodesIdentical (Seq.item i routeA.Nodes) (Seq.item i routeB.Nodes))
+    |> not
+
+let private areRouteSetsIdentical (routeSetA : RouteSet) (routeSetB : RouteSet) =
+    routeSetA.GetHashCode() = routeSetB.GetHashCode()
+    (* [0 .. Seq.length routeSetA.Routes - 1]
+    |> Seq.exists (fun i ->
+        not <| areRoutesIdentical (Seq.item i routeSetA.Routes) (Seq.item i routeSetB.Routes))
+    |> not *)
+
+[<Test>]
+[<Category("TppRouteSet")>]
+let ``one route with one node and one event should have original values when read`` () =
+    let random = new System.Random()
+    let routeSet = createRandomRouteSet random 1 1 1
+    use stream = new MemoryStream()
+    use writer = new BinaryWriter(stream)
+    RouteSet.Write routeSet (createWriteFunctions writer) |> ignore
+
+    stream.Position <- 0L
+
+    use reader = new BinaryReader(stream)
+    createReadFunctions reader
+    |> RouteSet.Read
+    |> fun readRouteSet -> areRouteSetsIdentical readRouteSet routeSet |> Assert.IsTrue
