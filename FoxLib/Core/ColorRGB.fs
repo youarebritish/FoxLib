e922b0e26a8ebf3715afae39ccd517b8d33704ca
Joey35233 <joey35233@gmail.com>
2018-04-10 13:59:31 -0400

    Revert "Revert "Implemented MaterialParamBinary support and a minor project file structure refactor""
    This reverts commit 3590f47c5a7e329f505fa34ea8fcb7d321d6b074.

---
+++ FoxLib/Core/ColorRGB.fs
@@ -0,0 +1,23 @@
+﻿module FoxLib.ColorRGB
+
+open FoxLib.Core
+
+/// <summary>
+/// Read a ColorRGB.
+/// </summary>
+/// <param name="readSingle">Function to read a float32.</param>
+/// <returns>The parsed ColorRGB.</returns>
+let internal Read readSingle = {
+    ColorRGB.Red = readSingle();
+    Green = readSingle();
+    Blue = readSingle();
+    }
+
+/// <summary>
+/// Write a ColorRGB.
+/// </summary>
+/// <param name="writeSingle">Function to write a float32.</param>
+let internal Write writeSingle (color : ColorRGB)  =
+    writeSingle color.Red
+    writeSingle color.Green
+    writeSingle color.Blue
