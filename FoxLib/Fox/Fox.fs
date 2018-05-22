c74d1d53773d2e6c79cbfa257beb1b224cee6011
youarebritish <bishop@projectbc.net>
2018-05-22 16:26:38 -0700

    Added StrCode32 and tested hashing functions.

--- FoxLib/Fox/Fox.fs
+++ FoxLib/Fox/Fox.fs
@@ -1,12 +1,33 @@
 ﻿module FoxLib.Fox
 
-let public StrCode (text : string) =
-    let seed0 = 0x9ae16a3b2f90404fUL
+open System.Text
+
+/// <summary>
+/// First of two seeds for the CityHash algorithm.
+/// </summary>
+let private cityHashSeed0 = 0x9ae16a3b2f90404fUL
+
+/// <summary>
+/// Calculates the StrCode hash of a string.
+/// </summary>
+/// <param name="text">The string to hash.</param>
+let public StrCode text =
+    if isNull text then nullArg text
+
     let seed1 = match text.Length with
                 | 0 -> 0UL
-                | _ ->  let term1 = (byte (text.Chars 0)) <<< 16
-                        let term2 = text.Length
-                        uint64 ((uint32 term1) + (uint32 term2))
-    
-    let cityHash = CityHash.CityHash.CityHash64WithSeeds(text + "\0", seed0, seed1)
-    cityHash &&& 0xFFFFFFFFFFFFUL
+                | _ ->  let term1 = uint32 (int text.[0] <<< 16)
+                        let term2 = uint32 text.Length
+                        term1 + term2
+                        |> uint64
+
+    CityHash.CityHash.CityHash64WithSeeds(text + "\000", cityHashSeed0, seed1)
+    |> (&&&) 0xFFFFFFFFFFFFUL
+
+/// <summary>
+/// Calculates the StrCode32 hash of a string.
+/// </summary>
+/// <param name="text">The string to hash.</param>
+let public StrCode32 text =
+    StrCode text
+    |> uint32
---
+++ TestFoxLib/Fox/Hashing.fs
@@ -0,0 +1,53 @@
+﻿module Fox.Hashing
+
+open NUnit.Framework
+open FoxLib
+open FoxLib.Fox
+
+[<Test>]
+[<Category("Fox.Hashing.StrCode")>]
+let ``StrCode of `hello` should be 14366034504703`` () =
+    let hash = StrCode "hello"
+    Assert.AreEqual(14366034504703UL, hash)
+
+[<Test>]
+[<Category("Fox.Hashing.StrCode")>]
+let ``StrCode of `hello world` should be 55535342535240`` () =
+    let hash = StrCode "hello world"
+    Assert.AreEqual(55535342535240UL, hash)
+
+[<Test>]
+[<Category("Fox.Hashing.StrCode")>]
+let ``StrCode of `123456789` should be 267500960428468`` () =
+    let hash = StrCode "123456789"
+    Assert.AreEqual(267500960428468UL, hash)
+
+[<Test>]
+[<Category("Fox.Hashing.StrCode")>]
+let ``StrCode of empty string should be 203000540209048`` () =
+    let hash = StrCode ""
+    Assert.AreEqual(203000540209048UL, hash)
+
+[<Test>]
+[<Category("Fox.Hashing.StrCode32")>]
+let ``StrCode32 of `hello` should be 3663866879`` () =
+    let hash = StrCode32 "hello"
+    Assert.AreEqual(3663866879u, hash)
+
+[<Test>]
+[<Category("Fox.Hashing.StrCode32")>]
+let ``StrCode32 of `hello world` should be 1415397960`` () =
+    let hash = StrCode32 "hello world"
+    Assert.AreEqual(1415397960u, hash)
+
+[<Test>]
+[<Category("Fox.Hashing.StrCode32")>]
+let ``StrCode32 of `123456789` should be 1807298996`` () =
+    let hash = StrCode32 "123456789"
+    Assert.AreEqual(1807298996u, hash)
+
+[<Test>]
+[<Category("Fox.Hashing.StrCode32")>]
+let ``StrCode32 of empty string should be 3205930904`` () =
+    let hash = StrCode32 ""
+    Assert.AreEqual(3205930904u, hash)
